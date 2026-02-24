fdd0616c73ef167c8b5c57c4f138ead24fcd60cf
Ayushi Kumari <marvellousayushi@gmail.com>
2026-02-24 23:12:26 +0530

    update

---
+++ server/controllers/salaryController.js
@@ -0,0 +1,26 @@
+import Salary  from "../models/Salary.js"
+
+const addSalary = async (req, res) => {
+    try {
+        const {employeeId, basicSalary, allowances, deductions, payDate} = req.body
+
+        const totalSalary = parseInt(basicSalary) + parseInt(allowances) - parseInt(deductions)
+
+        const newSalary = new Salary({
+            employeeId,
+            basicSalary,
+            allowances,
+            deductions,
+            netSalary: totalSalary,
+            payDate
+        })
+
+        await newSalary.save()
+
+        return res.status(200).json({success: true})
+    } catch (error) {
+        return res.status(500).json({success: false, error: "salary add server error"})
+    }
+}
+
+export {addSalary}
